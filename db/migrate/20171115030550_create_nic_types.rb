0b1f19cb5c24da515ee194f0cf92d93fdbe3d4fb
raccy <open@raccy.org>
2017-11-15 15:13:25 +0900

    indexなどを追加 後から付ける方針で行く

--- db/migrate/20171115030550_create_nic_types.rb
+++ db/migrate/20171115030550_create_nic_types.rb
@@ -1,9 +1,11 @@
 class CreateNicTypes < ActiveRecord::Migration[5.1]
   def change
     create_table :nic_types do |t|
-      t.string :name
+      t.string :name, null: false
       t.string :display_name
-      t.boolean :allow_laa
+      t.boolean :allow_laa, null: false, default: false
+
+      t.index :name, unique: true
 
       t.timestamps
     end
