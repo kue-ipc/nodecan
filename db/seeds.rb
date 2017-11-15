b90d5cf461738c37602cba5ea5a460eaf5fd48ad
raccy <open@raccy.org>
2017-11-15 14:46:53 +0900

    seedsを作ってみる

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'yaml'
+
+def read_seeds_yaml(model)
+  yaml_file = "#{Rails.root}/db/seeds/#{model.table_name}.yml"
+  list = YAML.load_file(yaml_file)
+  p list
+  model.create(list)
+end
+
+read_seeds_yaml(NicType)
