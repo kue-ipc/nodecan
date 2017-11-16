ab78fbbde701b281549e7f856ef853fb49ea35b9
raccy <open@raccy.org>
2017-11-16 17:17:22 +0900

    IPv4アドレスはやはりintegerにする。

--- db/migrate/20171116060151_create_ipv4_networks.rb
+++ db/migrate/20171116060151_create_ipv4_networks.rb
@@ -2,9 +2,9 @@ class CreateIpv4Networks < ActiveRecord::Migration[5.1]
   def change
     create_table :ipv4_networks do |t|
       t.references :network, foreign_key: true
-      t.binary :address, null: false, limit: 4
+      t.integer :address, null: false, limit: 5
       t.integer :netmask, null: false, limit: 1
-      t.binary :gateway, limit: 4
+      t.integer :gateway, limit: 5
       t.boolean :use_dhcp, null: false, default: false
       t.references :dhcp_type, foreign_key: true
 
--- db/migrate/20171116060214_create_ipv4_dhcp_pools.rb
+++ db/migrate/20171116060214_create_ipv4_dhcp_pools.rb
@@ -2,8 +2,8 @@ class CreateIpv4DhcpPools < ActiveRecord::Migration[5.1]
   def change
     create_table :ipv4_dhcp_pools do |t|
       t.references :ipv4_network, foreign_key: true
-      t.binary :first, null: false, limit: 4
-      t.binary :last, null: false, limit: 4
+      t.integer :first, null: false, limit: 5
+      t.integer :last, null: false, limit: 5
 
       t.timestamps
     end
