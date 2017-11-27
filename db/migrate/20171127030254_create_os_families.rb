46ad8f1f1f22152627e5fdb0f99613b70bcf537a
raccy <open@raccy.org>
2017-11-27 12:23:28 +0900

    require_security_softwareは製品毎管理

--- db/migrate/20171127030254_create_os_families.rb
+++ db/migrate/20171127030254_create_os_families.rb
@@ -2,7 +2,6 @@ class CreateOsFamilies < ActiveRecord::Migration[5.1]
   def change
     create_table :os_families do |t|
       t.string :name
-      t.boolean :require_security_software
 
       t.index :name, unique: true
 
