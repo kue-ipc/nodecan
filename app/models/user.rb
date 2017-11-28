f57fd2c4d8a51c066b9b4909262183f0783d8a14
raccy <open@raccy.org>
2017-11-28 14:40:35 +0900

    databaseもあるよということを残す

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :timeoutable and :omniauthable
-  # :registerable, :recoverable,
+  # :registerable, :recoverable, :database_authenticatable
   devise :ldap_authenticatable,
          :rememberable, :trackable, :validatable, :lockable
 end
