d65c85bdefb22220ca807c5b756c1b06384e17f7
raccy <open@raccy.org>
2017-11-29 13:46:51 +0900

    home#indexを追加し、rootに

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,6 @@ Rails.application.routes.draw do
   resources :nodes
   resources :networks
   devise_for :users
-  # devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # root to: "home#index"
+  root to: "home#index"
 end
