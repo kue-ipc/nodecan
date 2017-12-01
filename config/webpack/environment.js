f343ba4c26194d124ddeed6786d7312578afea38
raccy <open@raccy.org>
2017-12-01 22:38:09 +0900

    coffeescriptを使うようにする

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,14 @@
 const { environment } = require('@rails/webpacker')
+const merge = require('webpack-merge')
+
+const transpileCoffeeLoaderOptions = {
+  transpile: {
+    extends: `${__dirname}/../../.babelrc`
+  }
+}
+
+const CoffeeLoader = environment.loaders.get('coffee')
+
+CoffeeLoader.options = merge(CoffeeLoader.options, transpileCoffeeLoaderOptions)
 
 module.exports = environment
