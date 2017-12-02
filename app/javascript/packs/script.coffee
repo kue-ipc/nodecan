789be8485584c499e22365c137c3c8e2dec749ed
raccy <open@raccy.org>
2017-12-02 17:30:22 +0900

    bootstrapだけでも有効にする

---
+++ app/javascript/packs/script.coffee
@@ -0,0 +1,17 @@
+# import jQuery from 'jquery'
+# window.$ = jQuery
+# console.log($)
+# import 'popper.js'
+import 'bootstrap'
+import Bootbox from 'bootbox'
+@Bootbox = Bootbox
+console.log(@)
+window.Bootbox = Bootbox
+#
+# console.log(global)
+# console.log(window.confirm)
+# console.log('hello')
+#
+# window.confirm = (message) ->
+#   console.log(message)
+#   false
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,4 +1,5 @@
 const { environment } = require('@rails/webpacker')
+const webpack = require('webpack')
 const merge = require('webpack-merge')
 
 const transpileCoffeeLoaderOptions = {
@@ -10,5 +11,14 @@ const transpileCoffeeLoaderOptions = {
 const CoffeeLoader = environment.loaders.get('coffee')
 
 CoffeeLoader.options = merge(CoffeeLoader.options, transpileCoffeeLoaderOptions)
+
+environment.plugins.set(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: 'popper.js'
+  })
+)
 
 module.exports = environment
