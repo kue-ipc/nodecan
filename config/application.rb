3112d361933611b5badf06d3e10ab2e81e80c009
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2020-02-17 15:20:55 +0900

    5.2系へアップグレード

--- config/application.rb
+++ config/application.rb
@@ -12,9 +12,11 @@ module Nodecan
     config.load_defaults 5.1
 
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-    config.sass.preferred_syntax = :sass
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
+
+    # config.sass.preferred_syntax = :sass
     config.i18n.default_locale = 'ja'
   end
 end
